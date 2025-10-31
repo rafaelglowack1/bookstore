23c9ae01839b560d0e17ffa257a93ca0d27a9264
rafaelglowack1 <149637380+rafaelglowack1@users.noreply.github.com>
2025-10-31 09:16:59 -0300

    adicionado autenticação no order

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -7,10 +7,8 @@ from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
 
     serializer_class = ProductSerializer
 
     def get_queryset(self):
-        return Product.objects.all().order_by("id")
+        return Product.objects.all().order_by("id") 
